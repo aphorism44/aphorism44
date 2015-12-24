b2179e7f19996dd89a85db1f754f778411dff41b
aphorism44 <d-jesse@comcast.net>
2015-12-23 21:21:35 -0600

    Delete connect_db.php
    don't include connex info in repository

--- blog/blogEdit/blogResources/connect_db.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $dbc = mysqli_connect('basicBlog.db.10204542.hostedresource.com', 'basicBlog', 'ttn9gdCl@', 'basicBlog') OR die(mysqli_connect_error());
-    mysqli_set_charset($dbc, 'utf-8');
-?>
